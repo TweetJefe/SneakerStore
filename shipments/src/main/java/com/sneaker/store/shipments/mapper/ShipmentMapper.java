260a4aa5e978f6c94a23e353015ecd4acdaa5696
Tweet Jefe <a.chavychalov@gmail.com>
2025-11-08 02:24:38 +0100

    чуть подправил

--- shipments/src/main/java/com/sneaker/store/shipments/mapper/ShipmentMapper.java
+++ shipments/src/main/java/com/sneaker/store/shipments/mapper/ShipmentMapper.java
@@ -3,9 +3,13 @@ package com.sneaker.store.shipments.mapper;
 import com.sneaker.store.shipments.dto.ShipmentDTO;
 import com.sneaker.store.shipments.model.Shipment;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ShipmentMapper{
+
+    ShipmentMapper INSTANCE = Mappers.getMapper(ShipmentMapper.class);
+
     ShipmentDTO toDTO (Shipment shipment);
     Shipment toEntity (ShipmentDTO shipmentDTO);
 }
